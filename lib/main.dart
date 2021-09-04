94fa9ee7656e5e7b44509ea6a20ab764b6820386
Asaek <Asaek60@gmail.com>
2021-09-04 17:36:12 -0500

    Se agrego el blur gradientoso

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Kyary Aniversario',
-      initialRoute: 'entrada',
+      initialRoute: 'inicio',
       routes: {
         'inicio': (_) => PrincipalPage(),
         'biografia': (_) => BiografiaScreen(),
--- lib/screens/principal_screen.dart
+++ lib/screens/principal_screen.dart
@@ -11,15 +11,25 @@ class PrincipalPage extends StatelessWidget {
   Widget build(BuildContext context) {
     // posterProvider.loadingPoster();
 
-    final size = MediaQuery.of(context).size;
-
+    // final size = MediaQuery.of(context).size;
+    final String posterSeleccionado =
+        Provider.of<PosterPrincipalProvider>(context).posterSeleccionado;
     return Scaffold(
       body: Container(
         width: double.infinity,
+        // color: Colors.red,
         child: Stack(
           alignment: Alignment.bottomCenter,
+          // fit: StackFit.expand,
           children: [
-            _BackGroundPrincipal(),
+            /* TODO Al parecer se interpolan las imagenes el background el 
+              junto con el ImageFiltered esta muy intersante esto
+            */
+
+            _BackGroundPrincipal(
+              posterPaper: posterSeleccionado,
+            ),
+
             // Expanded(child: SizedBox()),
             // Positioned(
             //   bottom: 0,
@@ -29,32 +39,92 @@ class PrincipalPage extends StatelessWidget {
             //     color: Colors.teal,
             //   ),
             // ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                // TODO El ClipRRect corta el blur  que genial
-                BackdropFilter(
-                  filter: ImageFilter.blur(sigmaX: 0, sigmaY: 0),
-                  child: Container(
-                    height: 300,
-                    // decoration: BoxDecoration(
-                    //     color: Color.fromRGBO(62, 66, 107, 0.7),
-                    //     borderRadius: BorderRadius.circular(20)),
-                    child: Container(
-                      width: size.width,
-                      // height: 120,
-                      padding: EdgeInsets.only(top: 170, bottom: 10),
-                      // color: Colors.deepOrange,
-                      child: _ImagenSemanalListView(),
-                    ),
-                  ),
-                ),
-                _BotonMenuLuz(),
-              ],
+            // Container(
+            //   width: double.infinity,
+            //   height: 500,
+            //   decoration: BoxDecoration(
+            //     shape: ,
+            //     gradient: LinearGradient(
+            //       begin: Alignment.topCenter,
+            //       end: Alignment.bottomCenter,
+            //       // stops: [0.2, 0.8],
+            //       colors: [
+            //         Color.fromRGBO(0, 0, 0, 0),
+            //         Color.fromRGBO(32, 42, 48, 1),
+            //       ],
+            //     ),
+            //   ),
+            // ),
+            _BlurGradientoso(
+              posterPaper: posterSeleccionado,
             ),
+
+            // Container(
+            //     decoration: BoxDecoration(
+            //         gradient: LinearGradient(
+            //             begin: Alignment.topCenter,
+            //             end: Alignment.bottomCenter,
+            //             colors: [
+            //       Colors.black.withOpacity(0),
+            //       Colors.black.withOpacity(0.6)
+            //     ],
+            //             stops: [
+            //       0.3,
+            //       0.8
+            //     ]))),
+
+            Container(
+              width: double.infinity,
+              height: 100,
+
+              // padding: EdgeInsets.only(top: 180, bottom: 5),
+
+              child: _ImagenSemanalListView(),
+            ),
+            // _ImagenSemanalListView(),
+            // _BotonMenuLuz(),
           ],
         ),
       ),
+    );
+  }
+}
+
+class _BlurGradientoso extends StatelessWidget {
+  final String posterPaper;
+
+  const _BlurGradientoso({required this.posterPaper});
+  @override
+  Widget build(BuildContext context) {
+    return ImageFiltered(
+      imageFilter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+      child: ShaderMask(
+          shaderCallback: (rect) {
+            return LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+                Colors.black,
+                Colors.black.withOpacity(0),
+              ],
+              // stops: [0.4, 0.75],
+            ).createShader(rect);
+          },
+          /* TODO Aqui me que revisando como finciona linear gradiiente y como se apliican las
+          3 capas
+        */
+          blendMode: BlendMode.dstOut,
+          child: (posterPaper == '')
+              ? Image(
+                  image: AssetImage('assets/KYARY_loading.gif'),
+                  fit: BoxFit.cover,
+                  height: double.infinity, //Fantamas si muevves el tama;o
+                )
+              : Image(
+                  image: NetworkImage(posterPaper),
+                  fit: BoxFit.cover,
+                  height: double.infinity, //Fantamas si muevves el tama;o
+                )),
     );
   }
 }
@@ -109,11 +179,23 @@ class _ImagenSemanalListView extends StatelessWidget {
       itemCount: posterProvider.kyarPostersList.length,
       itemBuilder: (_, int index) => GestureDetector(
         child: Container(
+          width: 100,
+          height: 90,
+          // padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
           // padding: EdgeInsets.all(10),
-          width: 200,
-          height: 50,
+          margin: EdgeInsets.symmetric(horizontal: 5, vertical: 10),
+          decoration: BoxDecoration(
+            // color: Colors.deepOrange,
+            boxShadow: [
+              BoxShadow(
+                blurRadius: 5,
+                spreadRadius: 0.5,
+                offset: Offset(0, 0),
+              )
+            ],
+          ),
           child: ClipRRect(
-            borderRadius: BorderRadius.circular(15),
+            borderRadius: BorderRadius.circular(5),
             child: FadeInImage(
               placeholder: AssetImage('assets/KYARY_loading.gif'),
               image:
@@ -133,20 +215,23 @@ class _ImagenSemanalListView extends StatelessWidget {
   }
 }
 
+// TODO ese se poodria hacer con un gradiente que vaya de color a invicible
 class _BackGroundPrincipal extends StatelessWidget {
+  final String posterPaper;
+
+  const _BackGroundPrincipal({required this.posterPaper});
+
   @override
   Widget build(BuildContext context) {
-    final String posterSeleccionado =
-        Provider.of<PosterPrincipalProvider>(context).posterSeleccionado;
     return Container(
         height: double.infinity,
-        child: (posterSeleccionado == '')
+        child: (posterPaper == '')
             ? Image(
                 image: AssetImage('assets/KYARY_loading.gif'),
                 fit: BoxFit.cover,
               )
             : Image(
-                image: NetworkImage(posterSeleccionado),
+                image: NetworkImage(posterPaper),
                 fit: BoxFit.cover,
               )
 
